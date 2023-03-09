33aab8d02c5512c5073348ec8ece61d5a04420e2
YiboDan <42585395+danyibo@users.noreply.github.com>
2023-03-09 21:54:55 +0800

    Add files via upload

--- 容器.cpp
+++ 容器.cpp
@@ -310,6 +310,16 @@ namespace AllContainerAndAlgorithm {
 		PRINT_ELEMENTS(coll2, "squared: ");
 
 	}
+	void test_lambda() {
+		std::deque<int> coll = { 1, 3, 19, 5, 7, 11, 2, 17 };
+		int x = 5;
+		int y = 12;
+
+		auto pos = std::find_if(coll.cbegin(), coll.cend(),
+			[](int i) {return i > x && i < y; }
+		);
+		std::cout << "first elem > 5 and < 12  " << *pos << std::endl;
+	}
 };
 
 
@@ -333,4 +343,5 @@ int main() {
 	AllContainerAndAlgorithm::test_remove();
 	AllContainerAndAlgorithm::test_for_each();
 	AllContainerAndAlgorithm::test_transform();
+	AllContainerAndAlgorithm::test_lambda();
 }
